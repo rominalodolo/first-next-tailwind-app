e3e110beea0c6d28f6a468680edccbdadc2665d5
Romina Lodolo <lodoloromina@gmail.com>
2023-03-17 15:06:32 +0200

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-pink-300">
+      <h1 className="text-pink-300 border-pink-500 bg-slate-400">
         Hello world!
       </h1>
-      <h2 className="border-purple-400">
+      <h2 className="text-brandingMain ">
         My name is Romina
       </h2>
   </div>
