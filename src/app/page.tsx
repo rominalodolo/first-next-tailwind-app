68794dd3139358c229c1304da30dbbb101f9e64f
Romina Lodolo <lodoloromina@gmail.com>
2023-03-17 15:13:25 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-pink-300 border-pink-500 bg-slate-400">
+      <h1 className="text-pink-300 border-pink-500 border-width: 8px bg-slate-400">
         Hello world!
       </h1>
       <h2 className="text-brandingMain ">
