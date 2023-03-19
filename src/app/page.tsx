202386ef55d893cf664ed9706f8da244096b821f
Romina Lodolo <lodoloromina@gmail.com>
2023-03-19 21:57:48 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,25 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div>
-      <h1>
-        Hello world!
+      <h1 class="text-9xl">
+        Heading 1
       </h1>
-      <h2>
-        My name is Romina
+      <h2 class="text-7xl">
+        Second Header
       </h2>
+      <h3 class="text-4xl">
+        Another Heading
+      </h3>
+      <h4 class="text-lg">
+        And I guess another one? 
+      </h4>
+      <p class="text-sm">
+        Lastly, a p tag.
+      </p>
+      <p className="text-xs note">
+        Wait, another paragraph tag? 
+      </p>
+
   </div>
     
   )
