d42f0859612a13dbe06c5527dbd141a51e230803
Romina Lodolo <lodoloromina@gmail.com>
2023-03-19 20:13:42 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-pink-300 border-8 border-x-fuchsia-300 bg-slate-400">
+      <h1>
         Hello world!
       </h1>
-      <h2 className="text-brandingMain bg-[#bada55]">
+      <h2>
         My name is Romina
       </h2>
   </div>
