853a076ffc9cae67ea07e2a867a518c5cb9f14fd
Romina Lodolo <lodoloromina@gmail.com>
2023-03-19 22:54:44 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,7 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>
-      <h1 class="text-9xl">
-        Heading 1
-      </h1>
-      <h2 class="text-7xl">
-        Second Header
-      </h2>
-      <h3 class="text-4xl">
-        Another Heading
-      </h3>
-      <h4 class="text-lg">
-        And I guess another one? 
-      </h4>
-      <p class="text-sm">
-        Lastly, a p tag.
-      </p>
-      <p className="text-xs note">
-        Wait, another paragraph tag? 
-      </p>
-
-  </div>
+    <div className="font-mono pl-8 text-brandingMain bg-slate-500">Ciao</div>
     
   )
 }
